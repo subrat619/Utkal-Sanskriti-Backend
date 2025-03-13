12ff50450f02a45a40183d502e3ae80e9df61a93
subrat619 <cyfrifprotech.subrat@gmail.com>
2025-03-13 09:48:12 +0530

    ghghgh

--- src/main/java/com/cyfrifpro/config/WebSocketConfig.java
+++ src/main/java/com/cyfrifpro/config/WebSocketConfig.java
@@ -20,6 +20,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:5500") // specify allowed origin(s)
 				.withSockJS();
-
 	}
 }
